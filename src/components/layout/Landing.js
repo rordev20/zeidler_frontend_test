f66e39465c7837730386c4850c94594409991fa6
Rohit Patel <rohit.patel@infibeam.net>
2020-03-05 21:32:19 +0530

    frontend structure

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">User Connector</h1>
+          <p className="lead">
+            Create user and search
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Landing;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <a href="index.html">
+          <i className="fas fa-code">
+          </i> Zeidler
+        </a>
+      </h1>
+      <ul>
+        <li><a href="profiles.html">Users</a></li>
+        <li><a href="register.html">Register</a></li>
+        <li><a href="login.html">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
