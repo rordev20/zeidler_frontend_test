debd363c53ff79ff57a942fa1cc7fdc4038b6cc5
Rohit Patel <rohit.patel@infibeam.net>
2020-03-06 11:22:05 +0530

    added routing to application

---
+++ src/components/auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Login = () => {
+  return (
+    <div>
+      login
+    </div>
+  )
+}
+
+export default Login;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html">
+        <Link to="/">
           <i className="fas fa-code">
           </i> Zeidler
-        </a>
+        </Link>
       </h1>
       <ul>
-        <li><a href="profiles.html">Users</a></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </nav>
   )
