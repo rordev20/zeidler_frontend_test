f909e67e1ac3e74a11a75ac9de6c4130adee9678
Rohit Patel <rohit.patel@infibeam.net>
2020-03-06 17:36:03 +0530

    added registration and signup form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,11 +1,119 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    age: '',
+    gender: '',
+    country_id: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { first_name, last_name, age, gender, country_id, email, password, password2 } = formData;
+
+  const onChange = e =>
+        setFormData({...formData, [e.target.name]: e.target.value});
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('password do not match')
+    } else {
+      console.log('success')
+    }
+  }
+
   return (
-    <div>
-      register
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="First Name"
+            name="first_name"
+            value={first_name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="last_name"
+            value={last_name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="number"
+            placeholder="Age"
+            name="age"
+            value={age}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <select name="gender" value={gender} onChange={e => onChange(e)} >
+            <option value="0">* Select Gender</option>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <select name="country_id" value={country_id} onChange={e => onChange(e)}>
+            <option value="0">* Select Country</option>
+            <option value="1">India</option>
+            <option value="2">UK</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            value={password2}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </Fragment>
   )
 }
 
